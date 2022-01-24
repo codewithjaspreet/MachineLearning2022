fbd494bab943b5c573929c6f179a53365f294fad
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-01-24 22:50:58 +0530

    Indexing & Selection in Numpy

---
+++ Numpy/Indexing&Selection.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+arr = np.arange(0, 11)
+print(arr[8])
+
+print(arr[:])
+
+print(arr[1:5])
+
+print(arr[:5])
+
+print(arr[5:])
+
+# broadcasting
+
+arr[0:5] = 100
+print(arr)
+
+r = np.array([[5, 0, 15], [20, 25, 30], [25, 40, 40]])
+
+print(r.shape)
+
+print(r[0])
+print(r[2])
+
+print(r[1][1])
+# or
+print(r[1, 1])
+
+print(r[:2])
+print(r[:2, 1:])
+
+# conditional selections
+mp = np.arange(1, 11)
+
+print(mp)
+print(mp[mp > 4])
