cb007cb28244b1796e6865461881ada54f1f5ec9
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-01-27 10:34:22 +0530

    Condtional Filterings

--- Pandas/ConditionalFiltering.py
+++ Pandas/ConditionalFiltering.py
@@ -12,3 +12,16 @@ print(df)
 print(df['Pop'] > 50)  # perform conditional filtering
 print(df[df['Pop'] > 50])  # make changes in original dataframe
 
+# multiple conditional filterings
+
+# and  - >  both conditions need to be true
+# or - > either of the conditions
+print(df[(df['Year'] > 1868) & (df['Pop'] > 100)])
+#  | pipe operator for or
+
+options = [20.5 , 1.22]
+
+print(df[df['GDP'].isin(options)])
+# isin method faster way of checking two or more conditions
+
+
