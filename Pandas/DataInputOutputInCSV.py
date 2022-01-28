a7f5d771d0f72a60aada52ac235471e3b7a7e8ea
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-01-28 18:37:45 +0530

    Input Outputs In pandas

--- Pandas/DataInputOutputInCSV.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-
-print(os.getcwd())  # for getting directory working in
-
-df = pd.read_csv('example.csv', index_col=0)
-print(df)
-# df.to_csv('location to save' ,index=False)
